8ac939a17a2318660647d817ad017cae999aebef
Tifany <tifsnunes@gmail.com>
2024-06-02 11:16:00 -0300

    add: dark mode

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,30 +9,35 @@ import {
 } from "~/components/ui/dropdown-menu"
 import { Button } from "~/components/ui/button"
 import { PlusIcon } from "@radix-ui/react-icons"
+import { ModeToggle } from "./mode-toggle"
 
 export function Nav() {
 	return (
-		<div className="flex items-center justify-between">
-			<div>app</div>
-			<div className="flex gap-4">
-				<Button>
-					<PlusIcon className="mr-2 h-4 w-4" /> Nova Lista
-				</Button>
+		<nav className="border-b bg-card">
+			<div className="flex items-center justify-between p-4">
+				<div>app</div>
+				<div className="flex gap-4">
+					<Button>
+						<PlusIcon className="mr-2 h-4 w-4" /> Nova Lista
+					</Button>
 
-				<DropdownMenu>
-					<DropdownMenuTrigger>
-						<Avatar>
-							<AvatarImage src="https://github.com/pinuya.png" />
-							<AvatarFallback>TA</AvatarFallback>
-						</Avatar>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent>
-						<DropdownMenuLabel>Minha conta</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>Sair</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+					<ModeToggle />
+
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+							<Avatar>
+								<AvatarImage src="https://github.com/pinuya.png" />
+								<AvatarFallback>TA</AvatarFallback>
+							</Avatar>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>Sair</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
 			</div>
-		</div>
+		</nav>
 	)
 }
--- app/root.tsx
+++ app/root.tsx
@@ -4,9 +4,13 @@ import {
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	useLoaderData,
 } from "@remix-run/react"
 import "../tailwind.css"
-import type { LinksFunction } from "@remix-run/node"
+import clsx from "clsx"
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
+import { themeSessionResolver } from "./cookies"
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node"
 
 export const links: LinksFunction = () => {
 	return [
@@ -30,28 +34,41 @@ export const links: LinksFunction = () => {
 	]
 }
 
-export function Layout({ children }: { children: React.ReactNode }) {
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { getTheme } = await themeSessionResolver(request)
+	return {
+		theme: getTheme(),
+	}
+}
+
+export default function AppWithProviders() {
+	const data = useLoaderData<typeof loader>()
 	return (
-		<html lang="en">
+		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+			<App />
+		</ThemeProvider>
+	)
+}
+
+export function App() {
+	const data = useLoaderData<typeof loader>()
+	const [theme] = useTheme()
+
+	return (
+		<html lang="en" className={clsx(theme)}>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
+				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
 				<Links />
 			</head>
-			<body>
-				{children}
+			<body className="font-poppins">
+				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
 			</body>
 		</html>
-	)
-}
-
-export default function App() {
-	return (
-		<div>
-			<Outlet />
-		</div>
 	)
 }
