15401bd54e5f82b834379b7434da0f12a70e49d0
Tifany <tifsnunes@gmail.com>
2024-06-03 18:42:04 -0300

    add: nav landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node"
+import { Plus } from "lucide-react"
+import { ModeToggle } from "~/components/mode-toggle"
+import { Button } from "~/components/ui/button"
 
 export const meta: MetaFunction = () => {
 	return [
@@ -11,5 +14,34 @@ export const meta: MetaFunction = () => {
 }
 
 export default function Index() {
-	return <div className="font-yeseva p-3">Teste</div>
+	return (
+		<div>
+			<nav className="border-b bg-card">
+				<div className="flex items-center justify-between p-4">
+					<div className="flex flex-1 items-center justify-center">
+						<ul className="inline-flex space-x-6 font-bold">
+							<li>Sobre</li>
+							<li>Como usar</li>
+							<li>Contato</li>
+						</ul>
+					</div>
+
+					<div className="space-x-4">
+						<ModeToggle />
+
+						<Button variant="outline">Entrar</Button>
+
+						<Button>
+							<Plus className="mr-2 h-4 w-4" /> Criar Conta
+						</Button>
+					</div>
+				</div>
+			</nav>
+
+			<section>
+				<h2>Listify</h2>
+				<p>abuble abuble</p>
+			</section>
+		</div>
+	)
 }
