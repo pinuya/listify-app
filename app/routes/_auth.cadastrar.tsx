f35f190aeba7b5d0ec14da90fb05352aa32ad3fc
Tifany Alves <tifsnunes@gmail.com>
2024-06-11 19:36:13 -0300

    add: registration skeleton (#2)

--- app/routes/_auth.cadastrar.tsx
+++ app/routes/_auth.cadastrar.tsx
@@ -1,3 +1,29 @@
+import { Input } from "~/components/ui/input"
+
 export default function SignUp() {
-	return <div>Cadastrar</div>
+	return (
+		<div className="grid grid-cols-2">
+			<div>imagem bem bacana</div>
+
+			<div className="mx-auto flex w-full flex-col justify-center space-y-6">
+				<h2 className="text-2xl font-semibold tracking-tight">
+					Crie sua conta
+				</h2>
+				<p className="text-sm text-muted-foreground">
+					Registre-se, facil e rapido
+				</p>
+
+				<div className="grid gap-6">
+					<form>
+						<div className="grid gap-2">
+							<Input />
+							<Input />
+							<Input />
+							<Input />
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
