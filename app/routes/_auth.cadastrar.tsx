942b03edcde318b97ef3f0744784724f46d342e1
Tifany <tifsnunes@gmail.com>
2024-06-12 19:32:59 -0300

    style: inputs style

--- app/routes/_auth.cadastrar.tsx
+++ app/routes/_auth.cadastrar.tsx
@@ -1,3 +1,4 @@
+import { Button } from "~/components/ui/button"
 import { Input } from "~/components/ui/input"
 
 export default function SignUp() {
@@ -6,20 +7,55 @@ export default function SignUp() {
 			<div>imagem bem bacana</div>
 
 			<div className="mx-auto flex w-full flex-col justify-center space-y-6">
-				<h2 className="text-2xl font-semibold tracking-tight">
-					Crie sua conta
-				</h2>
-				<p className="text-sm text-muted-foreground">
-					Registre-se, facil e rapido
-				</p>
+				<div className="flex flex-col space-y-2 text-center">
+					<h2 className="text-2xl font-semibold tracking-tight">
+						Crie sua conta
+					</h2>
+					<p className="text-sm text-muted-foreground">
+						Registre-se, facil e rapido
+					</p>
+				</div>
 
 				<div className="grid gap-6">
 					<form>
 						<div className="grid gap-2">
-							<Input />
-							<Input />
-							<Input />
-							<Input />
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								id="nome"
+								placeholder="Nome"
+								autoCapitalize="none"
+								autoComplete="nome"
+								autoCorrect="off"
+								type="text"
+							/>
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								id="sobrenome"
+								placeholder="Sobrenome"
+								autoCapitalize="none"
+								autoComplete="sobrenome"
+								autoCorrect="off"
+								type="text"
+							/>
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								id="email"
+								placeholder="example@example.com"
+								autoCapitalize="none"
+								autoComplete="email"
+								autoCorrect="off"
+								type="email"
+							/>
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								id="senha"
+								placeholder="Senha"
+								autoCapitalize="none"
+								autoComplete="sobrenome"
+								autoCorrect="off"
+								type="password"
+							/>
+							<Button className="bg-foreground">Criar conta</Button>
 						</div>
 					</form>
 				</div>
