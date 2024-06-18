bfa8a2c9711b03722ed9646ec18d0bac07b6e299
Tifany <tifsnunes@gmail.com>
2024-06-18 18:23:12 -0300

    add: footer

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -121,6 +121,37 @@ export default function Index() {
 					</div>
 				</div>
 			</section>
+
+			<footer className="m-4">
+				<div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+					<div className="sm:flex sm:items-center sm:justify-between">
+						<ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+							<li>
+								<Link to={"#"} className="hover:underline me-4 md:me-6">
+									Sobre
+								</Link>
+							</li>
+
+							<li>
+								<Link to={"#"} className="hover:underline me-4 md:me-6">
+									Contato
+								</Link>
+							</li>
+
+							<li>
+								<Link to={"#"} className="hover:underline me-4 md:me-6">
+									Politicas de Privacidade
+								</Link>
+							</li>
+						</ul>
+					</div>
+
+					<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+					<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+						© 2024 Listify. Todos os direitos reservados
+					</span>
+				</div>
+			</footer>
 		</div>
 	)
 }
