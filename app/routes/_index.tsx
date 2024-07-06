3aa881671ca8df6aa4f027355fbfc1573548eb8b
Tifany <tifsnunes@gmail.com>
2024-07-06 08:40:12 -0300

    add: new section in landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -122,11 +122,32 @@ export default function Index() {
 				</div>
 			</section>
 
+			<section id="sobre">
+				<div className="flex flex-col pt-10 sm:flex-row">
+					<motion.div className="sm:mt-44">
+						<motion.h1
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ duration: 1 }}
+							className="font-semibold text-3xl sm:text-8xl">
+							Como usar
+							<br /> o Listify?
+						</motion.h1>
+						<p className="flex">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+							ut harum non quae velit exercitationem ipsum officiis libero fuga
+							qui. Necessitatibus, sunt velit! Nisi blanditiis ratione, commodi
+							sint quia minima?
+						</p>
+					</motion.div>
+				</div>
+			</section>
+
 			<footer className="m-4">
 				<div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 					<ul className="flex flex-wrap items-center justify-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
 						<li>
-							<Link to={"#"} className="hover:underline me-4 md:me-6">
+							<Link to={"#sobre"} className="hover:underline me-4 md:me-6">
 								Sobre
 							</Link>
 						</li>
