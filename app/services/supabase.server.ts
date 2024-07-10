1f48fc58fbf1c813dc4d84bb02d22988720cb47d
Tifany <tifsnunes@gmail.com>
2024-07-10 14:48:04 -0300

    add: setup supabase server client

---
+++ app/services/supabase.server.ts
@@ -0,0 +1,25 @@
+import { createServerClient, parse, serialize } from "@supabase/ssr"
+
+export const createSupabaseServerClient = (request: Request, admin = false) => {
+	const cookies = parse(request.headers.get("Cookie") ?? "")
+	const headers = new Headers()
+	const supabaseClient = createServerClient(
+		process.env.API_URL,
+		admin ? process.env.SERVICE_ROLE_KEY : process.env.ANON_KEY,
+		{
+			cookies: {
+				get(key) {
+					return cookies[key]
+				},
+				set(key, value, options) {
+					headers.append("Set-Cookie", serialize(key, value, options))
+				},
+				remove(key, options) {
+					headers.append("Set-Cookie", serialize(key, "", options))
+				},
+			},
+		},
+	)
+
+	return { supabaseClient, headers }
+}
---
+++ app/utils/env.server.ts
@@ -0,0 +1,40 @@
+import { z } from "zod"
+
+const schema = z.object({
+	NODE_ENV: z.enum(["production", "development", "test"] as const),
+	API_URL: z.string(),
+	JWT_SECRET: z.string(),
+	ANON_KEY: z.string(),
+	SERVICE_ROLE_KEY: z.string(),
+})
+
+type ValidatedEnvs = z.infer<typeof schema>
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends ValidatedEnvs {}
+	}
+}
+
+export function init() {
+	const parsed = schema.safeParse(process.env)
+
+	if (parsed.success === false) {
+		throw new Error("Invalid environment variables")
+	}
+}
+
+export function getEnv() {
+	return {
+		MODE: process.env.NODE_ENV,
+	}
+}
+
+type Env = ReturnType<typeof getEnv>
+
+declare global {
+	var ENV: Env
+	interface Window {
+		ENV: Env
+	}
+}
