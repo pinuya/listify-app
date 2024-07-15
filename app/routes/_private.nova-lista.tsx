2dd0768657913403988b1511129746ce7bbd2627
Tifany <tifsnunes@gmail.com>
2024-07-15 20:52:49 -0300

    add: remove item logic

--- app/routes/_private.nova-lista.tsx
+++ app/routes/_private.nova-lista.tsx
@@ -4,7 +4,19 @@ import { useState } from "react"
 import { Button } from "~/components/ui/button"
 
 export default function NewList() {
-	const [itemCounter, setItemCounter] = useState(1)
+	const [itemCounter, setItemCounter] = useState([1])
+
+	const removeItem = (v: number) => {
+		setItemCounter((prev) => prev.filter((value) => value !== v))
+	}
+
+	const addItem = () => {
+		if (itemCounter.length === 0) {
+			setItemCounter([1])
+			return
+		}
+		setItemCounter((prev) => [...prev, prev[prev.length - 1] + 1])
+	}
 
 	return (
 		<div className="grid gap-6">
@@ -19,21 +31,18 @@ export default function NewList() {
 					type="text"
 					placeholder="Descricao"
 				/>
-				{[...new Array(itemCounter)].map((_, i) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-					<div key={i} className="flex">
+				{itemCounter.map((v) => (
+					<div key={v} className="flex">
 						<input
 							className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 							type="text"
 							placeholder="item"
 						/>
-						<Trash2 />
+						<Trash2 onClick={() => removeItem(v)} />
 					</div>
 				))}
 
-				<Button
-					type="button"
-					onClick={() => setItemCounter((prev) => prev + 1)}>
+				<Button type="button" onClick={() => addItem()}>
 					Adicionar
 				</Button>
 
