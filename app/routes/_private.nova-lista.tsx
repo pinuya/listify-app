c6faae4cfe15352404194223937222f5bfe53493
Tifany <tifsnunes@gmail.com>
2024-07-16 20:30:29 -0300

    add: list form action

--- app/routes/_private.nova-lista.tsx
+++ app/routes/_private.nova-lista.tsx
@@ -1,9 +1,48 @@
-import { Form } from "@remix-run/react"
+import { useForm } from "@conform-to/react"
+import { parseWithZod } from "@conform-to/zod"
+import { type ActionFunctionArgs, json } from "@remix-run/node"
+import { Form, useActionData } from "@remix-run/react"
 import { Trash2 } from "lucide-react"
 import { useState } from "react"
+import { z } from "zod"
 import { Button } from "~/components/ui/button"
 
+const listSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	items: z.array(z.string()),
+})
+
+export const action = async (args: ActionFunctionArgs) => {
+	const formData = await args.request.formData()
+	const submission = parseWithZod(formData, { schema: listSchema })
+
+	if (submission.status !== "success") {
+		return json(submission.reply())
+	}
+
+	const { title, description, items } = submission.value
+	console.log(title, description, items)
+
+	return {}
+}
 export default function NewList() {
+	const lastResult = useActionData<typeof action>()
+
+	const [form, fields] = useForm({
+		// Sync the result of last submission
+		lastResult,
+
+		// Reuse the validation logic on the client
+		onValidate({ formData }) {
+			return parseWithZod(formData, { schema: listSchema })
+		},
+
+		// Validate the form on blur event triggered
+		shouldValidate: "onBlur",
+		shouldRevalidate: "onInput",
+	})
+
 	const [itemCounter, setItemCounter] = useState([1])
 
 	const removeItem = (v: number) => {
@@ -20,16 +59,26 @@ export default function NewList() {
 
 	return (
 		<div className="grid gap-6">
-			<Form method="POST" className="grid gap-2">
+			<Form
+				method="POST"
+				className="grid gap-2"
+				id={form.id}
+				onSubmit={form.onSubmit}>
 				<input
 					className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 					type="text"
 					placeholder="Titulo"
+					key={fields.title.key}
+					name={fields.title.name}
+					defaultValue={fields.title.initialValue}
 				/>
 				<input
 					className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 					type="text"
 					placeholder="Descricao"
+					key={fields.description.key}
+					name={fields.description.name}
+					defaultValue={fields.description.initialValue}
 				/>
 				{itemCounter.map((v) => (
 					<div key={v} className="flex">
@@ -37,6 +86,8 @@ export default function NewList() {
 							className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 							type="text"
 							placeholder="item"
+							key={fields.items.key}
+							name={fields.items.name}
 						/>
 						<Trash2 onClick={() => removeItem(v)} />
 					</div>
