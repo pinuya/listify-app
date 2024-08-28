24bc6c31d9c2f141fb7a0faf49db582397a3eba4
Tifany <tifsnunes@gmail.com>
2024-08-28 15:52:45 -0300

    add: new login page

--- app/routes/_auth.entrar.tsx
+++ app/routes/_auth.entrar.tsx
@@ -1,12 +1,10 @@
 import { useForm } from "@conform-to/react"
 import { parseWithZod } from "@conform-to/zod"
+import { Checkbox } from "@radix-ui/react-checkbox"
 import { Label } from "@radix-ui/react-label"
 import type { ActionFunctionArgs } from "@remix-run/node"
 import { Form, Link, json, redirect, useActionData } from "@remix-run/react"
 import { z } from "zod"
-import { Login } from "~/assets/images"
-// import { Login } from "~/assets/images"
-// import { Footer } from "~/components/Footer"
 import { Button } from "~/components/ui/button"
 import { createSupabaseServerClient } from "~/services/supabase.server"
 
@@ -69,27 +67,31 @@ export default function SignIn() {
 	})
 
 	return (
-		<div className="flex-col gap-4 h-screen items-center justify-center md:grid lg:max-none lg:grid-cols-2">
-			<div className="container">
-				<div className="p-4 mr-4 rounded-xl border bg-card text-card-foreground shadow mx-auto flex w-full flex-col justify-center space-y-6">
-					<div className="flex flex-col space-y-2 text-center p-6">
-						<div className="text-2xl font-semibold tracking-tight">
-							Faça o seu login.
-						</div>
-						<div className="text-sm text-muted-foreground">
-							Entre com seu e-mail e senha abaixo
-						</div>
-					</div>
-
-					<div className="grid gap-6">
+		<div className="flex min-h-[100dvh] items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+			<div className="x-auto w-full max-w-md space-y-8">
+				<div>
+					<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-foreground">
+						Entre com a sua conta
+					</h2>
+					<p className="mt-2 text-center text-sm text-muted-foreground">
+						Ou{" "}
+						<Link
+							to={"/cadastrar"}
+							className="font-medium text-primary hover:text-primary/90">
+							registre uma nova conta
+						</Link>
+					</p>
+					<div>
 						<Form
 							method="POST"
-							className="grid gap-2"
+							className="space-y-6"
 							id={form.id}
 							onSubmit={form.onSubmit}>
-							<Label htmlFor="email">E-mail</Label>
+							<Label htmlFor="email" className="sr-only">
+								E-mail
+							</Label>
 							<input
-								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								className="relative block w-full appearance-none rounded-md border border-input bg-background px-3 py-2 text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
 								type="email"
 								placeholder="example@example.com"
 								key={fields.email.key}
@@ -97,29 +99,36 @@ export default function SignIn() {
 								defaultValue={fields.email.initialValue}
 							/>
 							<p>{fields.email.errors}</p>
-							<Label htmlFor="password">Senha</Label>
+
 							<input
-								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								className="relative block w-full appearance-none rounded-md border border-input bg-background px-3 py-2 text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
 								type="password"
+								placeholder="Senha"
 								key={fields.password.key}
 								name={fields.password.name}
 								defaultValue={fields.password.initialValue}
 							/>
+
+							<div className="flex justify-end">
+								<div className="text-sm">
+									<Link
+										to={""}
+										className="font-medium text-primary hover:text-primary/90">
+										Esqueceu a senha?
+									</Link>
+								</div>
+							</div>
+
 							<p>{fields.password.errors}</p>
-							<Button type="submit" className="bg-foreground">
+							<Button
+								type="submit"
+								className="flex w-full justify-center rounded-md bg-primary py-2 px-4 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
 								Entrar
 							</Button>
-							<Link
-								className="text-sm text-muted-foreground hover:underline"
-								to={"/cadastrar"}>
-								Ainda não possui uma conta? Cadastre-se!
-							</Link>
 						</Form>
 					</div>
 				</div>
 			</div>
-
-			<img className="h-full w-full object-fill" src={Login} alt="create img" />
 		</div>
 	)
 }
