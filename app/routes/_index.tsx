434838db180e6cf67470b4dfc5b1dee29ea0c248
Tifany <tifsnunes@gmail.com>
2024-08-28 19:08:08 -0300

    add: link buttons in landing page to /sign in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -50,7 +50,7 @@ export default function Index() {
 						</p>
 						<div className="mt-6">
 							<Link
-								to={""}
+								to={"/cadastrar"}
 								className="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
 								Comece agora!
 							</Link>
@@ -170,7 +170,9 @@ export default function Index() {
 									placeholder="Enter your email"
 									className="max-w-lg flex-1"
 								/>
-								<Button type="submit">Cadastre-se</Button>
+								<Link to={"/cadastrar"}>
+									<Button type="submit">Cadastre-se</Button>
+								</Link>
 							</form>
 						</div>
 					</div>
