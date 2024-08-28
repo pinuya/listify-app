ebb2de60e17a02eee01b6a1c37c81865921061c4
Tifany <tifsnunes@gmail.com>
2024-08-28 18:53:05 -0300

    add: new sign up page

--- app/routes/_auth.cadastrar.tsx
+++ app/routes/_auth.cadastrar.tsx
@@ -65,74 +65,81 @@ export default function SignUp() {
 	})
 
 	return (
-		<div className="flex-col gap-4 h-screen items-center justify-center md:grid lg:max-none lg:grid-cols-2">
-			<img
-				className="h-full w-full object-fill"
-				src={CreateAcc}
-				alt="create img"
-			/>
-			<div className="container">
-				<div className="p-4 mr-4 rounded-xl border bg-card text-card-foreground shadow mx-auto flex w-full flex-col justify-center space-y-6">
-					<div className="flex flex-col space-y-2 text-center p-6">
-						<h2 className="text-2xl font-semibold tracking-tight">
-							Crie sua conta
-						</h2>
-					</div>
+		<div className="flex min-h-[100dvh] items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+			<div className="w-full max-w-md space-y-8">
+				<div>
+					<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-foreground">
+						Crie sua conta
+					</h2>
 
-					<div className="grid gap-6">
-						<div>
-							<Form
-								method="POST"
-								className="grid gap-2"
-								id={form.id}
-								onSubmit={form.onSubmit}>
-								<Label htmlFor="name">Nome</Label>
-								<input
-									className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-									type="text"
-									key={fields.name.key}
-									name={fields.name.name}
-									defaultValue={fields.name.initialValue}
-								/>
-								<p>{fields.name.errors}</p>
-								<Label htmlFor="lastName">Sobrenome</Label>
-								<input
-									className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-									type="text"
-									key={fields.lastName.key}
-									name={fields.lastName.name}
-									defaultValue={fields.lastName.initialValue}
-								/>
-								<p>{fields.lastName.errors}</p>
-								<Label htmlFor="email">E-mail</Label>
-								<input
-									className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-									type="email"
-									placeholder="example@example.com"
-									key={fields.email.key}
-									name={fields.email.name}
-									defaultValue={fields.email.initialValue}
-								/>
-								<p>{fields.email.errors}</p>
-								<Label htmlFor="password">Senha</Label>
-								<input
-									className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-									type="password"
-									key={fields.password.key}
-									name={fields.password.name}
-									defaultValue={fields.password.initialValue}
-								/>
-								<p>{fields.password.errors}</p>
-								<Button type="submit" className="bg-foreground">
-									Criar conta
-								</Button>
-								<Link
-									className="text-sm text-muted-foreground hover:underline"
-									to={"/entrar"}>
-									Já possui um cadastro? Entre aqui
-								</Link>
-							</Form>
-						</div>
+					<p className="mt-2 text-center text-sm text-muted-foreground">
+						Ou{" "}
+						<Link
+							to={"/entrar"}
+							className="font-medium text-primary hover:underline">
+							entre na sua conta.
+						</Link>
+					</p>
+				</div>
+
+				<div className="grid gap-6">
+					<div>
+						<Form
+							method="POST"
+							className="space-y-4"
+							id={form.id}
+							onSubmit={form.onSubmit}>
+							<div className="grid grid-cols-2 gap-4">
+								<div className="space-y-2">
+									<Label htmlFor="name">Nome</Label>
+									<input
+										className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+										type="text"
+										placeholder="John"
+										key={fields.name.key}
+										name={fields.name.name}
+										defaultValue={fields.name.initialValue}
+									/>
+								</div>
+
+								<div className="space-y-2">
+									<p>{fields.name.errors}</p>
+									<Label htmlFor="lastName">Sobrenome</Label>
+									<input
+										className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+										type="text"
+										placeholder="Doe"
+										key={fields.lastName.key}
+										name={fields.lastName.name}
+										defaultValue={fields.lastName.initialValue}
+									/>
+								</div>
+							</div>
+
+							<p>{fields.lastName.errors}</p>
+							<Label htmlFor="email">E-mail</Label>
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								type="email"
+								placeholder="example@example.com"
+								key={fields.email.key}
+								name={fields.email.name}
+								defaultValue={fields.email.initialValue}
+							/>
+							<p>{fields.email.errors}</p>
+							<Label htmlFor="password">Senha</Label>
+							<input
+								className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+								type="password"
+								key={fields.password.key}
+								name={fields.password.name}
+								defaultValue={fields.password.initialValue}
+							/>
+							<p>{fields.password.errors}</p>
+							<Button type="submit" className="w-full bg-foreground">
+								Criar conta
+							</Button>
+						</Form>
 					</div>
 				</div>
 			</div>
